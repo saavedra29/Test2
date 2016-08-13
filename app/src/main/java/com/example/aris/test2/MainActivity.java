165092ee482fc0f9f24198d3c98b316bde8936f7
saavedra29 <arisgold29@gmail.com>
2016-08-14 01:01:03 +0300

    main activity added non functional spinner for stages number

--- app/src/main/java/com/example/aris/test2/MainActivity.java
+++ app/src/main/java/com/example/aris/test2/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -14,6 +16,12 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Create the spinner
+        Spinner spinner = (Spinner) findViewById(R.id.stages_spinner);
+        ArrayAdapter<CharSequence> dataAdapter =ArrayAdapter.createFromResource(this,
+                R.array.stages_array, android.R.layout.simple_spinner_item);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
     }
 
     public void goToGame(View view)
