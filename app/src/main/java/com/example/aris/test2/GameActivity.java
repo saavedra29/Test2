5bf2a6f1595312467cd403d76f5e36668624c19d
saavedra29 <arisgold29@gmail.com>
2016-08-20 01:03:03 +0300

    game logic ready

--- app/src/main/java/com/example/aris/test2/GameActivity.java
+++ app/src/main/java/com/example/aris/test2/GameActivity.java
@@ -21,6 +21,7 @@ public class GameActivity extends Activity
     private ArrayList<Integer> couple = new ArrayList<>();
     private ArrayList<String> animalList;
     private HashMap<Integer, Integer> relation = new HashMap<>();
+    private HashMap<Integer, Boolean> faceCondition = new HashMap<>();
     private ViewGroup layoutView;
     static int round;
     static String score;
@@ -48,75 +49,80 @@ public class GameActivity extends Activity
 
     public void imageClick(View view)
     {
-        ImageButton button1 = null;
-        ImageButton button2 = null;
-        int imageId1 = 0;
-        int imageId2 = 1;
-        // Method called when an imageButton is pressed
 
-        // Check the stage
-        if ((!gameState) && (!couple.contains(view.getId())))
+        ImageButton currentButton = (ImageButton)view;
+        if (!faceCondition.get(currentButton.getId()))
         {
-            // Check the couple for same elements
-            for (Map.Entry<Integer, Integer> entry: relation.entrySet())
+            ImageButton button1 = null;
+            ImageButton button2 = null;
+            int imageId1 = 0;
+            int imageId2 = 1;
+            // Method called when an imageButton is pressed
+
+            // Check the stage
+            if (!gameState)
             {
-                int buttonId = entry.getKey();
-                if (buttonId == couple.get(0))
+                // Check the couple for same elements
+                for (Map.Entry<Integer, Integer> entry: relation.entrySet())
                 {
-                    button1 = (ImageButton)findViewById(buttonId);
-                    int imageId = entry.getValue();
-                    imageId1 = imageId;
+                    int buttonId = entry.getKey();
+                    if (buttonId == couple.get(0))
+                    {
+                        button1 = (ImageButton)findViewById(buttonId);
+                        int imageId = entry.getValue();
+                        imageId1 = imageId;
+                    }
+
+                    if (buttonId == couple.get(1))
+                    {
+                        button2 = (ImageButton)findViewById(buttonId);
+                        int imageId = entry.getValue();
+                        imageId2 = imageId;
+                    }
                 }
 
-                if (buttonId == couple.get(1))
+                if (imageId1 == imageId2)
                 {
-                    button2 = (ImageButton)findViewById(buttonId);
-                    int imageId = entry.getValue();
-                    imageId2 = imageId;
+                    // Set the imageButtons invisible
+                    assert button1 != null;
+                    button1.setVisibility(View.INVISIBLE);
+                    invisibleObjects++;
+                    assert button2 != null;
+                    button2.setVisibility(View.INVISIBLE);
+                    invisibleObjects++;
                 }
-            }
 
-            if (imageId1 == imageId2)
-            {
-                // Set the imageButtons invisible
-                assert button1 != null;
-                button1.setVisibility(View.INVISIBLE);
-                invisibleObjects++;
-                assert button2 != null;
-                button2.setVisibility(View.INVISIBLE);
-                invisibleObjects++;
-            }
-
-            // Change couple images back to cover
-            for (int i = 1; i >= 0; i--)
-            {
-                ImageButton button = (ImageButton)findViewById(couple.get(i));
-                if (button != null)
+                // Change couple images back to cover
+                for (int i = 1; i >= 0; i--)
                 {
-                    button.setImageResource(getResources().getIdentifier("cover", "drawable",
-                            getPackageName()));
+                    ImageButton button = (ImageButton)findViewById(couple.get(i));
+                    if (button != null)
+                    {
+                        button.setImageResource(getResources().getIdentifier("cover", "drawable",
+                                getPackageName()));
+                        faceCondition.put(button.getId(), false);
+                    }
                 }
-            }
-            couple.clear();
-            // Add current selected image to couple
-            couple.add(0, view.getId());
-            // Turn the current image
-            ImageButton tmpButton = (ImageButton)view;
-            int tmpImageId = relation.get(view.getId());
-            tmpButton.setImageResource(tmpImageId);
-            gameState = true;
-
-        }
-        else
-        {
-            // Check if already pressed
-            int id = view.getId();
-            if (id != couple.get(0));
-            {
+                couple.clear();
+                // Add current selected image to couple
+                couple.add(0, view.getId());
                 // Turn the current image
                 ImageButton tmpButton = (ImageButton)view;
                 int tmpImageId = relation.get(view.getId());
                 tmpButton.setImageResource(tmpImageId);
+                faceCondition.put(currentButton.getId(), true);
+                gameState = true;
+
+            }
+            else
+            {
+                // Check if already pressed
+                int id = view.getId();
+                // Turn the current image
+                ImageButton tmpButton = (ImageButton)view;
+                int tmpImageId = relation.get(view.getId());
+                tmpButton.setImageResource(tmpImageId);
+                faceCondition.put(currentButton.getId(), true);
                 // Add it to couple second position
                 couple.add(1, id);
                 // Check for end of game
@@ -135,6 +141,7 @@ public class GameActivity extends Activity
                 gameState = false;
             }
         }
+
     }
 
     private void initStage()
@@ -178,6 +185,7 @@ public class GameActivity extends Activity
             nextChild.setImageResource(getResources().getIdentifier(
                     "cover", "drawable", getPackageName()));
             relation.put(nextChild.getId(), resID);
+            faceCondition.put(nextChild.getId(), false);
 
         }
     }
