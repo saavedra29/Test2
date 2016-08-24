505adc50810b9515a16e258b7c9e53c448aaed77
saavedra29 <arisgold29@gmail.com>
2016-08-24 23:31:17 +0300

    added reset buttons to highscores

--- app/src/main/java/com/saavedra29/aris/test2/HighscoresActivity.java
+++ app/src/main/java/com/saavedra29/aris/test2/HighscoresActivity.java
@@ -3,6 +3,7 @@ package com.saavedra29.aris.test2;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class HighscoresActivity extends AppCompatActivity
@@ -27,5 +28,55 @@ public class HighscoresActivity extends AppCompatActivity
         TextView v5 = (TextView)findViewById(R.id.highscore5output);
         v5.setText(preferences.getString("5_str", "00:00:0"));
 
+    }
+
+    public void onReset1(View view)
+    {
+        SharedPreferences preferences = getApplicationContext().getSharedPreferences("Highscores",
+                MODE_PRIVATE);
+        preferences.edit().remove("1").apply();
+        preferences.edit().remove("1_str").apply();
+        TextView v = (TextView)findViewById(R.id.highscore1output);
+        v.setText("00:00:0");
+    }
+
+    public void onReset2(View view)
+    {
+        SharedPreferences preferences = getApplicationContext().getSharedPreferences("Highscores",
+                MODE_PRIVATE);
+        preferences.edit().remove("2").apply();
+        preferences.edit().remove("2_str").apply();
+        TextView v = (TextView)findViewById(R.id.highscore2output);
+        v.setText("00:00:0");
+    }
+
+    public void onReset3(View view)
+    {
+        SharedPreferences preferences = getApplicationContext().getSharedPreferences("Highscores",
+                MODE_PRIVATE);
+        preferences.edit().remove("3").apply();
+        preferences.edit().remove("3_str").apply();
+        TextView v = (TextView)findViewById(R.id.highscore3output);
+        v.setText("00:00:0");
+    }
+
+    public void onReset4(View view)
+    {
+        SharedPreferences preferences = getApplicationContext().getSharedPreferences("Highscores",
+                MODE_PRIVATE);
+        preferences.edit().remove("4").apply();
+        preferences.edit().remove("4_str").apply();
+        TextView v = (TextView)findViewById(R.id.highscore4output);
+        v.setText("00:00:0");
+    }
+
+    public void onReset5(View view)
+    {
+        SharedPreferences preferences = getApplicationContext().getSharedPreferences("Highscores",
+                MODE_PRIVATE);
+        preferences.edit().remove("5").apply();
+        preferences.edit().remove("5_str").apply();
+        TextView v = (TextView)findViewById(R.id.highscore5output);
+        v.setText("00:00:0");
     }
 }
